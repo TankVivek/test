b79530acc3577e6b67af82836bc579e1f985d10a
Tank Vivek <112782531+TankVivek@users.noreply.github.com>
2023-03-18 22:13:03 +0530

    Add files via upload

---
+++ 18.03.2023/model/user.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  pass: {
+    type: String,
+  },
+  Created: {
+    type: Date,
+    default: Date.now(),
+  },
+  img: {
+    type: String,
+  },
+  Tokens: [
+    {
+      token: {
+        type: String,
+      },
+    },
+  ],
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("pass")) {
+      this.pass = await bcrypt.hash(this.pass, 10);
+    }
+  } catch (error) {
+    console.log("bcryptErr :", error);
+  }
+});
+
+UserSchema.method.generateToken = async function (next) {
+  try {
+    const Skey = "thisismysecretkeyforlogin";
+    const token = await jwt.sign({ _id: this._id }, Skey);
+
+    this.Tokens = await this.Tokens.concat({ token: token });
+    await this.save();
+    // return token;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = new mongoose.model("User", UserSchema);
